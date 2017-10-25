22093c06eccf7b07fc4489a17489d21404039b49
Grégoire Geis <public.greg@outlook.com>
2017-10-25 23:05:32 +0200

    Slightly improved docs and structure of *Value traits

--- src/values/enums.rs
+++ src/values/enums.rs
@@ -2,7 +2,7 @@ use llvm_sys::core::{LLVMTypeOf, LLVMGetTypeKind};
 use llvm_sys::LLVMTypeKind;
 use llvm_sys::prelude::LLVMValueRef;
 
-use types::BasicTypeEnum;
+use types::{AnyTypeEnum, BasicTypeEnum};
 use values::traits::AsValueRef;
 use values::{IntValue, FunctionValue, PointerValue, VectorValue, ArrayValue, StructValue, FloatValue, PhiValue, InstructionValue, MetadataValue};
 
@@ -43,6 +43,40 @@ enum_value_set! {AnyValueEnum: ArrayValue, IntValue, FloatValue, PhiValue, Funct
 enum_value_set! {BasicValueEnum: ArrayValue, IntValue, FloatValue, PointerValue, StructValue, VectorValue}
 enum_value_set! {BasicMetadataValueEnum: ArrayValue, IntValue, FloatValue, PointerValue, StructValue, VectorValue, MetadataValue}
 
+impl AnyValueEnum {
+    pub(crate) fn new(value: LLVMValueRef) -> AnyValueEnum {
+        let type_kind = unsafe {
+            LLVMGetTypeKind(LLVMTypeOf(value))
+        };
+
+        match type_kind {
+            LLVMTypeKind::LLVMFloatTypeKind |
+            LLVMTypeKind::LLVMFP128TypeKind |
+            LLVMTypeKind::LLVMDoubleTypeKind |
+            LLVMTypeKind::LLVMHalfTypeKind |
+            LLVMTypeKind::LLVMX86_FP80TypeKind |
+            LLVMTypeKind::LLVMPPC_FP128TypeKind => AnyValueEnum::FloatValue(FloatValue::new(value)),
+            LLVMTypeKind::LLVMIntegerTypeKind => AnyValueEnum::IntValue(IntValue::new(value)),
+            LLVMTypeKind::LLVMStructTypeKind => AnyValueEnum::StructValue(StructValue::new(value)),
+            LLVMTypeKind::LLVMPointerTypeKind => AnyValueEnum::PointerValue(PointerValue::new(value)),
+            LLVMTypeKind::LLVMArrayTypeKind => AnyValueEnum::ArrayValue(ArrayValue::new(value)),
+            LLVMTypeKind::LLVMVectorTypeKind => AnyValueEnum::VectorValue(VectorValue::new(value)),
+            LLVMTypeKind::LLVMFunctionTypeKind => AnyValueEnum::FunctionValue(FunctionValue::new(value).unwrap()),
+            LLVMTypeKind::LLVMVoidTypeKind => panic!("Void values shouldn't exist."),
+            LLVMTypeKind::LLVMMetadataTypeKind => panic!("Metadata values are not supported as AnyValue's."),
+            _ => panic!("The given type is not supported.")
+        }
+    }
+
+    pub fn get_type(&self) -> AnyTypeEnum {
+        let type_ = unsafe {
+            LLVMTypeOf(self.as_value_ref())
+        };
+
+        AnyTypeEnum::new(type_)
+    }
+}
+
 impl BasicValueEnum {
     pub(crate) fn new(value: LLVMValueRef) -> BasicValueEnum {
         let type_kind = unsafe {
@@ -61,7 +95,7 @@ impl BasicValueEnum {
             LLVMTypeKind::LLVMPointerTypeKind => BasicValueEnum::PointerValue(PointerValue::new(value)),
             LLVMTypeKind::LLVMArrayTypeKind => BasicValueEnum::ArrayValue(ArrayValue::new(value)),
             LLVMTypeKind::LLVMVectorTypeKind => BasicValueEnum::VectorValue(VectorValue::new(value)),
-            _ => unreachable!("Unsupported type"),
+            _ => unreachable!("The given type is not a basic type."),
         }
     }
 
@@ -81,6 +115,20 @@ impl BasicValueEnum {
             BasicValueEnum::StructValue(ref val) => val.as_instruction(),
             BasicValueEnum::PointerValue(ref val) => val.as_instruction(),
             BasicValueEnum::VectorValue(ref val) => val.as_instruction(),
+        }
+    }
+}
+
+impl AggregateValueEnum {
+    pub(crate) fn new(value: LLVMValueRef) -> AggregateValueEnum {
+        let type_kind = unsafe {
+            LLVMGetTypeKind(LLVMTypeOf(value))
+        };
+
+        match type_kind {
+            LLVMTypeKind::LLVMArrayTypeKind => AggregateValueEnum::ArrayValue(ArrayValue::new(value)),
+            LLVMTypeKind::LLVMStructTypeKind => AggregateValueEnum::StructValue(StructValue::new(value)),
+            _ => unreachable!("The given type is not an aggregate type."),
         }
     }
 }
