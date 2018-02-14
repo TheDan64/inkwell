eafca272063896e40d4a6dc109a3ea4af93239dd
Daniel Kolsoi <thadan64@gmail.com>
2018-02-13 21:23:05 -0500

    Added some conversion impls

--- src/values/enums.rs
+++ src/values/enums.rs
@@ -39,7 +39,7 @@ macro_rules! enum_value_set {
 }
 
 enum_value_set! {AggregateValueEnum: ArrayValue, StructValue}
-enum_value_set! {AnyValueEnum: ArrayValue, IntValue, FloatValue, PhiValue, FunctionValue, PointerValue, StructValue, VectorValue}
+enum_value_set! {AnyValueEnum: ArrayValue, IntValue, FloatValue, PhiValue, FunctionValue, PointerValue, StructValue, VectorValue, InstructionValue}
 enum_value_set! {BasicValueEnum: ArrayValue, IntValue, FloatValue, PointerValue, StructValue, VectorValue}
 enum_value_set! {BasicMetadataValueEnum: ArrayValue, IntValue, FloatValue, PointerValue, StructValue, VectorValue, MetadataValue}
 
@@ -154,5 +154,11 @@ impl BasicMetadataValueEnum {
             LLVMTypeKind::LLVMMetadataTypeKind => BasicMetadataValueEnum::MetadataValue(MetadataValue::new(value)),
             _ => unreachable!("Unsupported type"),
         }
+    }
+}
+
+impl From<BasicValueEnum> for AnyValueEnum {
+    fn from(value: BasicValueEnum) -> AnyValueEnum {
+        AnyValueEnum::new(value.as_value_ref())
     }
 }
