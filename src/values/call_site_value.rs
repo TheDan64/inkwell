017f631fb331a992afced65739e557bed1917960
Daniel Kolsoi <thadan64@gmail.com>
2018-10-16 23:57:15 -0400

    Cleaned up doc tests to be compatible with older LLVM versions

--- src/values/call_site_value.rs
+++ src/values/call_site_value.rs
@@ -32,8 +32,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
@@ -61,8 +59,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
@@ -92,8 +88,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
@@ -414,8 +408,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
@@ -443,8 +435,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
@@ -474,8 +464,6 @@ impl CallSiteValue {
     /// let void_type = context.void_type();
     /// let fn_type = void_type.fn_type(&[], false);
     /// let fn_value = module.add_function("my_fn", fn_type, None);
-    /// let string_attribute = context.create_string_attribute("my_key", "my_val");
-    /// let enum_attribute = context.create_enum_attribute(1, 1);
     /// let entry_bb = fn_value.append_basic_block("entry");
     ///
     /// builder.position_at_end(&entry_bb);
