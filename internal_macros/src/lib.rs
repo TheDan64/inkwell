072f94ad3458af3f0bad6eaa19d542b6cbd7fff7
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2019-06-28 01:09:51 -0400

    Refactor llvm_versions macro
    - Support ability to specify single versions
    - Provide more flexibility in extending syntax for version
    specifications in the future

--- internal_macros/src/lib.rs
+++ internal_macros/src/lib.rs
@@ -8,11 +8,13 @@ extern crate syn;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Arm, Expr, ExprLit, Item, Lit, punctuated::Pair, Pat, PatLit, parse_macro_input};
+use syn::parse_macro_input;
+use syn::parse::{Parse, ParseStream, Result, Error};
+use syn::{Token, LitFloat, Ident, Item};
 
 // We could include_str! inkwell's Cargo.toml and extract these features
 // so that they don't need to also be managed here...
-const ALL_FEATURE_VERSIONS: [&str; 8] = [
+const ALL_FEATURE_VERSIONS: [&str; 9] = [
     "llvm3-6",
     "llvm3-7",
     "llvm3-8",
@@ -21,25 +23,41 @@ const ALL_FEATURE_VERSIONS: [&str; 8] = [
     "llvm5-0",
     "llvm6-0",
     "llvm7-0",
+    "llvm8-0",
 ];
 
 fn panic_with_usage() -> ! {
-    panic!("llvm_versions must take the form: X.Y => (X.Y || latest)");
+    panic!("llvm_versions must take the form: X.Y or X.Y => (X.Y || latest)");
 }
 
-fn get_latest_feature() -> String {
-    ALL_FEATURE_VERSIONS[ALL_FEATURE_VERSIONS.len() - 1].into()
+fn get_latest_feature_index() -> usize {
+    ALL_FEATURE_VERSIONS.len() - 1
 }
 
-fn get_feature_slice(start_feature: &str, end_feature: &str) -> Option<&'static [&'static str]> {
-    let start_index = ALL_FEATURE_VERSIONS.iter().position(|&str| str == start_feature)?;
-    let end_index = ALL_FEATURE_VERSIONS.iter().position(|&str| str == end_feature)?;
-
-    if end_index < start_index {
-        return None;
+fn get_feature_slice(vt: VersionType) -> Option<&'static [&'static str]> {
+    match vt {
+        VersionType::Specific(version) => {
+            let feature = f64_to_feature_string(version);
+            let index = ALL_FEATURE_VERSIONS.iter().position(|&s| s == feature)?;
+            Some(&ALL_FEATURE_VERSIONS[index..index])
+        }
+        VersionType::RangeToLatest(version) => {
+            let latest = get_latest_feature_index();
+            let feature = f64_to_feature_string(version);
+            let index = ALL_FEATURE_VERSIONS.iter().position(|&s| s == feature)?;
+            Some(&ALL_FEATURE_VERSIONS[index..=latest])
+        }
+        VersionType::Range(start, end) => {
+            let start_feature = f64_to_feature_string(start);
+            let end_feature = f64_to_feature_string(end);
+            let start_index = ALL_FEATURE_VERSIONS.iter().position(|&s| s == start_feature)?;
+            let end_index = ALL_FEATURE_VERSIONS.iter().position(|&s| s == end_feature)?;
+            if end_index < start_index {
+                return None;
+            }
+            Some(&ALL_FEATURE_VERSIONS[start_index..=end_index])
+        }
     }
-
-    Some(&ALL_FEATURE_VERSIONS[start_index..=end_index])
 }
 
 fn f64_to_feature_string(float: f64) -> String {
@@ -48,29 +66,52 @@ fn f64_to_feature_string(float: f64) -> String {
     format!("llvm{}-{}", int, (float * 10.) % 10.)
 }
 
+enum VersionType {
+    Specific(f64),
+    RangeToLatest(f64),
+    Range(f64, f64),
+}
+impl Parse for VersionType {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(LitFloat) {
+            let from = input.parse::<LitFloat>().unwrap().value();
+            if input.is_empty() {
+                return Ok(VersionType::Specific(from));
+            }
+            let lookahead = input.lookahead1();
+            if lookahead.peek(Token![=>]) {
+                let _: Token![=>] = input.parse().unwrap();
+                let lookahead = input.lookahead1();
+                if lookahead.peek(Ident) {
+                    let to = input.parse::<Ident>().unwrap();
+                    if to.to_string() == "latest" {
+                        Ok(VersionType::RangeToLatest(from))
+                    } else {
+                        Err(Error::new(to.span(), "expected `latest`"))
+                    }
+                } else if lookahead.peek(LitFloat) {
+                    let to = input.parse::<LitFloat>().unwrap().value();
+                    Ok(VersionType::Range(from, to))
+                } else {
+                    Err(lookahead.error())
+                }
+            } else {
+                Err(lookahead.error())
+            }
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
 #[proc_macro_attribute]
 pub fn llvm_versions(attribute_args: TokenStream, attributee: TokenStream) -> TokenStream {
-    let arm = parse_macro_input!(attribute_args as Arm);
+    let vt = parse_macro_input!(attribute_args as VersionType);
 
-    if arm.pats.len() != 1 {
-        panic_with_usage();
-    }
+    let features = get_feature_slice(vt)
+        .unwrap_or_else(|| panic_with_usage());
 
-    let start_feature = match arm.pats.first().unwrap() {
-        Pair::End(Pat::Lit(PatLit { expr })) => match **expr {
-            Expr::Lit(ExprLit { lit: Lit::Float(ref literal_float), .. }) => f64_to_feature_string(literal_float.value()),
-            _ => panic_with_usage(),
-        },
-        _ => panic_with_usage(),
-    };
-    let end_feature = match *arm.body {
-        Expr::Lit(ExprLit { lit: Lit::Float(literal_float), .. }) => Some(f64_to_feature_string(literal_float.value())),
-        // We could assert the path is just "latest" but this seems like a lot of extra work...
-        Expr::Path(_) => None,
-        _ => panic_with_usage(),
-    }.unwrap_or_else(get_latest_feature);
-
-    let features = get_feature_slice(&start_feature, &end_feature).unwrap_or_else(|| panic_with_usage());
     let attributee: Item = syn::parse(attributee).expect("Could not parse attributed item");
 
     let q = quote! {
