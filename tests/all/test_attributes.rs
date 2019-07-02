7355a63e2eb0b7b04390660fee713154a5403ada
Nick Lewycky <nick@wasmer.io>
2019-07-02 10:34:15 -0700

    Fix test_attributes.
    test_enum_attributes was testing that it could create arbitrary enum attributes. LLVM doesn't permit that in general, you can only use attribute kinds that already exist. Remove this test.
    
    test_attributes_on_call_site_values was creating a function void(i32) but creating a call with zero arguments, which triggers an assertion failure in LLVM creating the call instruction with mismatching arguments. Create it with one argument, check that it has one argument not zero, and then the tests that attribute the first argument have an argument to apply an attribute to.

--- tests/all/test_attributes.rs
+++ tests/all/test_attributes.rs
@@ -57,34 +57,6 @@ fn test_enum_attribute_kinds() {
 }
 
 #[test]
-fn test_enum_attributes() {
-    let context = Context::create();
-    let enum_attribute = context.create_enum_attribute(0, 10);
-
-    assert!(enum_attribute.is_enum());
-    assert!(!enum_attribute.is_string());
-
-    assert_eq!(enum_attribute.get_enum_kind_id(), 0);
-    assert_eq!(enum_attribute.get_enum_value(), 10);
-
-    let enum_attribute2 = context.create_enum_attribute(3, 14);
-
-    assert!(enum_attribute2.is_enum());
-    assert!(!enum_attribute2.is_string());
-
-    assert_eq!(enum_attribute2.get_enum_kind_id(), 3);
-    assert_eq!(enum_attribute2.get_enum_value(), 14);
-
-    let enum_attribute3 = context.create_enum_attribute(56, 20);
-
-    assert!(enum_attribute3.is_enum());
-    assert!(!enum_attribute3.is_string());
-
-    assert_eq!(enum_attribute3.get_enum_kind_id(), 56);
-    assert_eq!(enum_attribute3.get_enum_value(), 20);
-}
-
-#[test]
 fn test_string_attributes() {
     let context = Context::create();
     let string_attribute = context.create_string_attribute("my_key_123", "my_val");
@@ -155,11 +127,11 @@ fn test_attributes_on_call_site_values() {
 
     builder.position_at_end(&entry_bb);
 
-    let call_site_value = builder.build_call(fn_value, &[], "my_fn");
+    let call_site_value = builder.build_call(fn_value, &[i32_type.const_int(1, false).into()], "my_fn");
 
     builder.build_return(None);
 
-    assert_eq!(call_site_value.count_arguments(), 0);
+    assert_eq!(call_site_value.count_arguments(), 1);
     assert_eq!(call_site_value.count_attributes(0), 0);
     assert_eq!(call_site_value.count_attributes(1), 0);
 
