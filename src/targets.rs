d050a0d42a799947d9681cc157d92a3e01b23e0f
Daniel Kolsoi <thadan64@gmail.com>
2020-06-09 10:02:17 -0400

    Added RISCV target support to LLVM 9+ Fixes #191

--- src/targets.rs
+++ src/targets.rs
@@ -743,14 +743,13 @@ impl Target {
         }
     }
 
-    // REVIEW: As it turns out; RISCV was accidentally built by default in 4.0 since
-    // it was meant to be marked experimental and so it was later removed from default
-    // builds in 5.0+. Since llvm-sys doesn't officially support any experimental targets
-    // we're going to make this 4.0 only for now so that it doesn't break test builds.
-    // We can revisit this issue if someone wants RISCV support in inkwell, or if
-    // llvm-sys starts supporting experimental llvm targets. See
-    // https://lists.llvm.org/pipermail/llvm-dev/2017-August/116347.html for more info
-    #[llvm_versions(4.0)]
+    // RISCV was accidentally built by default in 4.0 since it was meant to be marked
+    // experimental and so it was later removed from default builds in 5.0 until it was
+    // officially released in 9.0 Since llvm-sys doesn't officially support any experimental
+    // targets we're going to make this 9.0+ only. See
+    // https://lists.llvm.org/pipermail/llvm-dev/2017-August/116347.html for more info.
+    #[cfg(feature = "target-riscv")]
+    #[llvm_versions(9.0..=latest)]
     pub fn initialize_riscv(config: &InitializationConfig) {
         use llvm_sys::target::{
             LLVMInitializeRISCVTarget, LLVMInitializeRISCVTargetInfo, LLVMInitializeRISCVTargetMC,
