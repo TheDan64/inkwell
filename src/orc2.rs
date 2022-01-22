10f911a6d1558de03db1259987545e2a84b6bbfc
Jakob Leifhelm <jakob.leifhelm@gmail.com>
2022-01-22 18:24:23 +0100

    orc2: Add documentation

--- src/orc2.rs
+++ src/orc2.rs
@@ -38,6 +38,7 @@ use crate::{
     targets::{InitializationConfig, Target},
 };
 
+/// An LLVM Error.
 #[derive(Debug)]
 pub struct LLVMError {
     error: LLVMErrorRef,
@@ -64,10 +65,25 @@ impl LLVMError {
         // FIXME: Don't expose LLVMErrorTypeId
         unsafe { LLVMGetErrorTypeId(self.error) }
     }
+    /// Returns the error message of the error. This consumes the error
+    /// and makes the error unusable afterwards.
+    /// ```
+    /// use std::ffi::{CString, CStr};
+    /// use inkwell::orc2::LLVMError;
+    ///
+    /// let error = LLVMError::new_string_error("llvm error");
+    /// assert_eq!(*error.get_message(), *CString::new("llvm error").unwrap().as_c_str());
+    /// ```
     pub fn get_message(mut self) -> LLVMErrorMessage {
         self.handled = true;
         unsafe { LLVMErrorMessage::new(LLVMGetErrorMessage(self.error)) }
     }
+    /// Creates a new StringError with the given message.
+    /// ```
+    /// use inkwell::orc2::LLVMError;
+    ///
+    /// let error = LLVMError::new_string_error("string error");
+    /// ```
     pub fn new_string_error(message: &str) -> Self {
         let error = unsafe { LLVMCreateStringError(to_c_str(message).as_ptr()) };
         LLVMError {
@@ -85,6 +101,7 @@ impl Drop for LLVMError {
     }
 }
 
+/// An owned LLVM Error Message.
 #[derive(Eq)]
 pub struct LLVMErrorMessage {
     pub(crate) ptr: *const c_char,
@@ -157,6 +174,7 @@ impl Drop for LLVMErrorMessage {
     }
 }
 
+/// The thread safe variant of [`Context`] used in this module.
 #[derive(Debug)]
 pub struct ThreadSafeContext {
     thread_safe_context: LLVMOrcThreadSafeContextRef,
@@ -175,12 +193,39 @@ impl ThreadSafeContext {
             context,
         }
     }
+
+    /// Creates a new `ThreadSafeContext`.
+    /// ```
+    /// use inkwell::orc2::ThreadSafeContext;
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// ```
     pub fn create() -> Self {
         unsafe { ThreadSafeContext::new(LLVMOrcCreateNewThreadSafeContext()) }
     }
+
+    /// Gets the [`Context`] from this instance. The returned [`Context`]
+    /// can be used for most operations concerning the context.
+    /// ```
+    /// use inkwell::orc2::ThreadSafeContext;
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// let context = thread_safe_context.context();
+    /// ```
     pub fn context(&self) -> &Context {
         &self.context
     }
+
+    /// Creates a [`ThreadSafeModule`] from a [`Module`]. The [`Module`] is now
+    /// owned by the [`ThreadSafeModule`]
+    /// ```
+    /// use inkwell::orc2::ThreadSafeContext;
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// let context = thread_safe_context.context();
+    /// let module = context.create_module("main");
+    /// let thread_safe_module = thread_safe_context.create_module(module);
+    /// ```
     pub fn create_module<'ctx>(&'ctx self, module: Module<'ctx>) -> ThreadSafeModule<'ctx> {
         unsafe {
             ThreadSafeModule::new(
@@ -199,6 +244,9 @@ impl Drop for ThreadSafeContext {
     }
 }
 
+/// The thread safe variant of [`Module`] used in this module. The underlying
+/// llvm module will be disposed when this object is dropped, except when
+/// it is owned by an execution engine.
 #[derive(Debug)]
 pub struct ThreadSafeModule<'ctx> {
     thread_safe_module: LLVMOrcThreadSafeModuleRef,
@@ -227,6 +275,7 @@ impl<'ctx> Drop for ThreadSafeModule<'ctx> {
     }
 }
 
+/// Represents a reference to an LLVM `LLJIT` execution engine.
 #[derive(Debug, Clone)]
 pub struct LLJIT {
     lljit: Rc<LLVMOrcLLJITRef>,
@@ -239,6 +288,15 @@ impl LLJIT {
             lljit: Rc::new(lljit),
         }
     }
+
+    /// Creates a `LLJIT` instance with the provided `lljit_builder`.
+    /// If `lljit_builder` is `None` the default LLJITBuilder will be
+    /// used.
+    /// ```
+    /// use inkwell::orc2::LLJIT;
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// ```
     pub fn create() -> Result<Self, LLVMError> {
         unsafe { LLJIT::create_with_builder(null_mut()) }
     }
@@ -251,10 +309,32 @@ impl LLJIT {
         LLVMError::new(error)?;
         Ok(LLJIT::new(lljit))
     }
+
+    /// Returns the main [`JITDylib`].
+    /// ```
+    /// use inkwell::orc2::LLJIT;
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// ```
     pub fn get_main_jit_dylib(&self) -> JITDylib {
         unsafe { JITDylib::new(LLVMOrcLLJITGetMainJITDylib(*self.lljit), Some(self.clone())) }
     }
 
+    /// Adds `module` to `jit_dylib` in the execution engine.
+    /// ```
+    /// use inkwell::orc2::{LLJIT, ThreadSafeContext};
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// let context = thread_safe_context.context();
+    /// let module = context.create_module("main");
+    /// let thread_safe_module = thread_safe_context.create_module(module);
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// jit.add_module(&main_jd, thread_safe_module)
+    ///     .expect("LLJIT::add_module failed");
+    /// ```
     pub fn add_module<'ctx>(
         &self,
         jit_dylib: &JITDylib,
@@ -272,6 +352,21 @@ impl LLJIT {
         LLVMError::new(error)
     }
 
+    /// Adds `module` to `rt`'s JITDylib in the execution engine.
+    /// ```
+    /// use inkwell::orc2::{LLJIT, ThreadSafeContext};
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// let context = thread_safe_context.context();
+    /// let module = context.create_module("main");
+    /// let thread_safe_module = thread_safe_context.create_module(module);
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// let module_rt = main_jd.create_resource_tracker();
+    /// jit.add_module_with_rt(&module_rt, thread_safe_module)
+    ///     .expect("LLJIT::add_module_with_rt failed");
+    /// ```
     pub fn add_module_with_rt<'ctx>(
         &self,
         rt: &ResourceTracker,
@@ -289,6 +384,10 @@ impl LLJIT {
         LLVMError::new(error)
     }
 
+    /// Attempts to lookup a function by `name` in the execution engine.
+    ///
+    /// It is recommended to use [`get_function()`](LLJIT::get_function()) instead of this method when intending to call the function
+    /// pointer so that you don't have to do error-prone transmutes yourself.
     pub fn get_function_address(&self, name: &str) -> Result<u64, LLVMError> {
         let mut function_address = 0;
         let error = unsafe {
@@ -298,6 +397,60 @@ impl LLJIT {
         Ok(function_address)
     }
 
+    /// Attempts to lookup a function by `name` in the execution engine.
+    ///
+    /// The [`UnsafeFunctionPointer`] trait is designed so only `unsafe extern
+    /// "C"` functions can be retrieved via the `get_function()` method. If you
+    /// get funny type errors then it's probably because you have specified the
+    /// wrong calling convention or forgotten to specify the retrieved function
+    /// as `unsafe`.
+    ///
+    /// # Example
+    /// ```
+    /// use inkwell::orc2::{ThreadSafeContext, LLJIT};
+    ///
+    /// let thread_safe_context = ThreadSafeContext::create();
+    /// let context = thread_safe_context.context();
+    /// let module = context.create_module("main");
+    /// let builder = context.create_builder();
+    ///
+    /// // Set up the function signature
+    /// let double = context.f64_type();
+    /// let func_sig = double.fn_type(&[], false);
+    ///
+    /// // Add the function to our module
+    /// let func = module.add_function("test_fn", func_sig, None);
+    /// let entry_bb = context.append_basic_block(func, "entry");
+    /// builder.position_at_end(entry_bb);
+    ///
+    /// // Insert a return statement
+    /// let ret = double.const_float(64.0);
+    /// builder.build_return(Some(&ret));
+    ///
+    /// // Create the LLJIT engine and add the module
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let thread_safe_module = thread_safe_context.create_module(module);
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// jit.add_module(&main_jd, thread_safe_module)
+    ///     .expect("LLJIT::add_module_with_rt failed");
+    ///
+    /// // lookup the function and execute it
+    /// unsafe {
+    ///     let test_fn = jit.get_function::<unsafe extern "C" fn() -> f64>("test_fn")
+    ///         .expect("LLJIT::get_function failed");
+    ///     let return_value = test_fn.call();
+    ///     assert_eq!(return_value, 64.0);
+    /// }
+    /// ```
+    ///
+    /// # Safety
+    ///
+    /// It is the caller's responsibility to ensure they call the function with
+    /// the correct signature and calling convention.
+    ///
+    /// The [`Function`] wrapper ensures a function won't accidentally outlive the
+    /// execution engine it came from, but adding functions or removing resource trackers
+    /// after calling this method *may* invalidate the function pointer.
     pub unsafe fn get_function<F>(&self, name: &str) -> Result<Function<F>, LLVMError>
     where
         F: UnsafeFunctionPointer,
@@ -319,6 +472,7 @@ impl Drop for LLJIT {
     }
 }
 
+/// An `LLJITBuilder` is used to create custom [`LLJIT`] instances.
 #[derive(Debug)]
 pub struct LLJITBuilder {
     builder: LLVMOrcLLJITBuilderRef,
@@ -330,10 +484,22 @@ impl LLJITBuilder {
         LLJITBuilder { builder }
     }
 
+    /// Creates an `LLJITBuilder`.
+    /// ```
+    /// use inkwell::orc2::LLJITBuilder;
+    ///
+    /// let jit_builder = LLJITBuilder::create();
+    /// ```
     pub fn create() -> Self {
         LLJITBuilder::new(unsafe { LLVMOrcCreateLLJITBuilder() })
     }
 
+    /// Builds the [`LLJIT`] instance. Consumes the `LLJITBuilder`.
+    /// ```
+    /// use inkwell::orc2::LLJITBuilder;
+    ///
+    /// let jit_builder = LLJITBuilder::create();
+    /// let jit = jit_builder.build().expect("LLJITBuilder::build failed");
     pub fn build(mut self) -> Result<LLJIT, LLVMError> {
         let lljit = unsafe { LLJIT::create_with_builder(self.builder) }?;
         self.builder = null_mut();
@@ -348,6 +514,7 @@ impl Drop for LLJITBuilder {
     }
 }
 
+/// Represents a dynamic library in the execution engine.
 #[derive(Debug)]
 pub struct JITDylib {
     jit_dylib: LLVMOrcJITDylibRef,
@@ -363,6 +530,14 @@ impl JITDylib {
         }
     }
 
+    /// Returns the default [`ResourceTracker`].
+    /// ```
+    /// use inkwell::orc2::LLJIT;
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// let rt = main_jd.get_default_resource_tracker();
+    /// ```
     pub fn get_default_resource_tracker(&self) -> ResourceTracker {
         ResourceTracker::new(
             unsafe { LLVMOrcJITDylibGetDefaultResourceTracker(self.jit_dylib) },
@@ -371,6 +546,14 @@ impl JITDylib {
         )
     }
 
+    /// Creates a new [`ResourceTracker`].
+    /// ```
+    /// use inkwell::orc2::LLJIT;
+    ///
+    /// let jit = LLJIT::create().expect("LLJIT::create failed");
+    /// let main_jd = jit.get_main_jit_dylib();
+    /// let rt = main_jd.create_resource_tracker();
+    /// ```
     pub fn create_resource_tracker(&self) -> ResourceTracker {
         ResourceTracker::new(
             unsafe { LLVMOrcJITDylibCreateResourceTracker(self.jit_dylib) },
@@ -380,6 +563,8 @@ impl JITDylib {
     }
 }
 
+/// A `ResourceTracker` is used to add/remove JIT resources.
+/// Look at [`JITDylib`] for further information.
 #[derive(Debug)]
 pub struct ResourceTracker {
     rt: LLVMOrcResourceTrackerRef,
@@ -408,6 +593,8 @@ impl Drop for ResourceTracker {
     }
 }
 
+/// A wrapper around a function pointer which ensures the function being pointed
+/// to doesn't accidentally outlive its execution engine.
 #[derive(Debug)]
 pub struct Function<F> {
     func: F,
