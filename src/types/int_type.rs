9322b029c1709f00df7a9a9fef5ed99ff1956c7d
Folkert <folkert@folkertdev.nl>
2022-06-12 19:09:31 +0200

    formatting

--- src/types/int_type.rs
+++ src/types/int_type.rs
@@ -332,7 +332,6 @@ impl<'ctx> IntType<'ctx> {
         self.int_type.print_to_string()
     }
 
-
     // See Type::print_to_stderr note on 5.0+ status
     /// Prints the definition of an `IntType` to stderr. Not available in newer LLVM versions.
     #[llvm_versions(3.7..=4.0)]
