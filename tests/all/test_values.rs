78b7f748f5971bfd11a37d4bd7b453240735c96a
Daniel Kolsoi <ThaDan64@gmail.com>
2024-07-13 14:56:36 -0400

    commented out some tests

--- tests/all/test_values.rs
+++ tests/all/test_values.rs
@@ -873,7 +873,7 @@ fn test_value_from_string() {
     let f64_type = context.f64_type();
     let f64_val = f64_type.const_float_from_string("3.6");
 
-    // assert_eq!(f64_val.print_to_string().to_string(), "double 3.600000e+00");
+    assert_eq!(f64_val.print_to_string().to_string(), "double 3.600000e+00");
 
     // let f64_val = f64_type.const_float_from_string("3.");
 
