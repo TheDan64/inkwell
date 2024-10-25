fa49de7879663d753134b915646d24d00145ccc2
TheDan64 <TheDan64@users.noreply.github.com>
2024-10-25 15:20:54 +0000

    deploy: 7b410298b6a93450adaa90b1841d5805a3038f12

---
+++ search.desc/inkwell_internals/inkwell_internals-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("inkwell_internals", 0, "These macros are only intended to be used by inkwell …\nThis attribute macro allows you to decorate an enum …\nThis attribute is used to decorate enums, structs, or …\nThis macro can be used to specify version constraints for …")
