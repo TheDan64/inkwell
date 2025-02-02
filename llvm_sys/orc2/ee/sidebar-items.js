ea633922d027601a0aae75e41475afdb51ac9632
TheDan64 <TheDan64@users.noreply.github.com>
2025-02-02 05:00:34 +0000

    deploy: 97128a9036c24448a5bd2904238e0df064c6ea9d

---
+++ llvm_sys/orc2/ee/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["LLVMOrcCreateRTDyldObjectLinkingLayerWithMCJITMemoryManagerLikeCallbacks","LLVMOrcCreateRTDyldObjectLinkingLayerWithSectionMemoryManager","LLVMOrcRTDyldObjectLinkingLayerRegisterJITEventListener"],"type":["LLVMMemoryManagerCreateContextCallback","LLVMMemoryManagerNotifyTerminatingCallback"]};
