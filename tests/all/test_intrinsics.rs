ccef91f7a174977f4ded363f1f5f25b37cd5a24c
Steve Fan <steve.fan@n-hop.com>
2025-05-22 16:06:59 +0800

    disable test_get_decl_va_copy for LLVM 19 and up for now

--- tests/all/test_intrinsics.rs
+++ tests/all/test_intrinsics.rs
@@ -28,11 +28,16 @@ fn test_get_decl_cos() {
     assert_eq!(decl.get_name().to_str().unwrap(), "llvm.cos.f32");
 }
 
+#[llvm_versions(..19)]
 #[test]
 fn test_get_decl_va_copy() {
     let va_copy = Intrinsic::find("llvm.va_copy").unwrap();
 
-    assert!(!va_copy.is_overloaded());
+    // Looks like starting from LLVM 19, this is overloaded?
+    #[cfg(not(any(feature = "llvm19-1", feature = "llvm20-1")))]
+    {
+        assert!(!va_copy.is_overloaded());
+    }
 
     let context = Context::create();
     let module = context.create_module("my_module");
