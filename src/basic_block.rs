e29a3d068f35918316777508afcbc96b5c80537c
アイラ <mut@vip.qq.com>
2025-09-03 22:50:21 +0800

    feat: make BasicBlock::new publicly accessible (#601)
    * Update basic_block.rs
    
    * docs: Add documentation for BasicBlock::new method
    
    ---------
    
    Co-authored-by: Dan Kolsoi <ThaDan64@gmail.com>

--- src/basic_block.rs
+++ src/basic_block.rs
@@ -31,7 +31,12 @@ pub struct BasicBlock<'ctx> {
 }
 
 impl<'ctx> BasicBlock<'ctx> {
-    pub(crate) unsafe fn new(basic_block: LLVMBasicBlockRef) -> Option<Self> {
+    /// Create a basic block from an [LLVMBasicBlockRef].
+    ///
+    /// # Safety
+    ///
+    /// The ref must be valid and point to a valid LLVM basic block.
+    pub unsafe fn new(basic_block: LLVMBasicBlockRef) -> Option<Self> {
         if basic_block.is_null() {
             return None;
         }
