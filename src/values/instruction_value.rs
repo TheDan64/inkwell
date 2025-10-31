d72c3770e74df1c57c5912a0403ac4deda77f902
Nam Se Hyun <3580430+namse@users.noreply.github.com>
2025-10-31 13:23:46 +0900

    Linting

--- src/values/instruction_value.rs
+++ src/values/instruction_value.rs
@@ -13,9 +13,9 @@ use llvm_sys::core::{
     LLVMIsATerminatorInst, LLVMIsConditional, LLVMIsTailCall, LLVMSetAlignment, LLVMSetMetadata, LLVMSetOperand,
     LLVMSetVolatile, LLVMValueAsBasicBlock,
 };
-use llvm_sys::core::{LLVMGetOrdering, LLVMSetOrdering};
 #[llvm_versions(10..)]
 use llvm_sys::core::{LLVMGetAtomicRMWBinOp, LLVMIsAAtomicCmpXchgInst, LLVMIsAAtomicRMWInst};
+use llvm_sys::core::{LLVMGetOrdering, LLVMSetOrdering};
 use llvm_sys::prelude::LLVMValueRef;
 use llvm_sys::LLVMOpcode;
 
@@ -23,10 +23,10 @@ use std::{ffi::CStr, fmt, fmt::Display};
 
 use crate::error::AlignmentError;
 use crate::values::{BasicValue, BasicValueEnum, BasicValueUse, MetadataValue, Value};
-use crate::{basic_block::BasicBlock, types::AnyTypeEnum};
-use crate::{types::BasicTypeEnum, values::traits::AsValueRef};
 #[llvm_versions(10..)]
 use crate::AtomicRMWBinOp;
+use crate::{basic_block::BasicBlock, types::AnyTypeEnum};
+use crate::{types::BasicTypeEnum, values::traits::AsValueRef};
 use crate::{AtomicOrdering, FloatPredicate, IntPredicate};
 
 use super::AnyValue;
