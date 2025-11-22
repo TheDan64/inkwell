508de231c5d37433e8db0ee62b35ac29b8b73f66
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2025-11-22 12:52:02 -0800

    Fixed formatting (I hope)

--- src/values/instruction_value.rs
+++ src/values/instruction_value.rs
@@ -1020,9 +1020,7 @@ impl<'ctx> InstructionValue<'ctx> {
     #[llvm_versions(9..)]
     pub fn get_debug_location(self) -> Option<DILocation<'ctx>> {
         // https://github.com/llvm/llvm-project/blob/e83cc896e7c2378914a391f942c188d454b517d2/llvm/include/llvm/IR/Instruction.h#L513
-        let metadata_ref = unsafe {
-            llvm_sys::debuginfo::LLVMInstructionGetDebugLoc(self.as_value_ref())
-        };
+        let metadata_ref = unsafe { llvm_sys::debuginfo::LLVMInstructionGetDebugLoc(self.as_value_ref()) };
         if metadata_ref.is_null() {
             None
         } else {
@@ -1038,9 +1036,7 @@ impl<'ctx> InstructionValue<'ctx> {
     pub fn set_debug_location(self, location: Option<DILocation<'_>>) {
         // https://github.com/llvm/llvm-project/blob/e83cc896e7c2378914a391f942c188d454b517d2/llvm/include/llvm/IR/Instruction.h#L510
         let metadata_ref = location.map_or(std::ptr::null_mut(), |loc| loc.metadata_ref);
-        unsafe {
-            llvm_sys::debuginfo::LLVMInstructionSetDebugLoc(self.as_value_ref(), metadata_ref);
-        }
+        unsafe { llvm_sys::debuginfo::LLVMInstructionSetDebugLoc(self.as_value_ref(), metadata_ref); }
     }
 }
 
